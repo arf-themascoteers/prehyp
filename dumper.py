da009207ed1f4253f4941c8ee6de6d8a26c26dfb
Arif <arif@themascoteers.com>
2024-05-13 22:07:13 +1000

    Initial commit

---
+++ dumper.py
@@ -0,0 +1,29 @@
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import scipy.io as sio
+import pandas as pd
+
+
+def dump(X_file,gt_file,dump_file,X_key,gt_key):
+    X_img = sio.loadmat(X_file)
+    gt_img = sio.loadmat(gt_file)
+    X = X_img.get(X_key).astype('float64')
+    gt = gt_img.get(gt_key).astype('float64')
+
+    X = X.reshape(X.shape[0] * X.shape[1], X.shape[2])
+    gt = gt.reshape(-1)
+
+    mask = (gt!=0)
+
+    gt = gt[mask]
+    X = X[mask]
+
+    scaler = MinMaxScaler()
+    X = scaler.fit_transform(X)
+
+    gt = gt-1
+
+    columns = ["class"] + [str(i) for i in range(X.shape[1])]
+    data = np.concatenate((gt.reshape(-1,1),X), axis=1)
+    df = pd.DataFrame(columns=columns, data=data)
+    df.to_csv(dump_file, index=False)
---
+++ main.py
@@ -0,0 +1,4 @@
+import dumper
+
+if __name__ == '__main__':
+    dumper.dump("base_data/Indian_pines_corrected.mat", "base_data/Indian_pines_gt.mat", "data/indian_pines.csv",'indian_pines_corrected','indian_pines_gt')
