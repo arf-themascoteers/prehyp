dd355cb56061ed8d0a96b22110ca0b8b929551bc
aarahman <arif@themascoteers.com>
2024-06-27 20:34:20 +1000

    done

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ if __name__ == '__main__':
 
     # log = dumper.dump("base_data/Pavia.mat", "base_data/Pavia_gt.mat", "data/pavia.csv",'pavia','pavia_gt')
     # f.write(str(log)+"\n")
-    #
-    # log = dumper.dump("base_data/PaviaU.mat", "base_data/PaviaU_gt.mat", "data/paviaU.csv",'paviaU','paviaU_gt')
-    # f.write(str(log)+"\n")
-    #
-    # log = dumper.dump("base_data/Salinas_corrected.mat", "base_data/Salinas_gt.mat", "data/salinas.csv",'salinas_corrected','salinas_gt')
-    # f.write(str(log)+"\n")
-    #
+    # #
+    log = dumper.dump("base_data/PaviaU.mat", "base_data/PaviaU_gt.mat", "data/paviaU.csv",'paviaU','paviaU_gt')
+    f.write(str(log)+"\n")
+
+    log = dumper.dump("base_data/Salinas_corrected.mat", "base_data/Salinas_gt.mat", "data/salinas.csv",'salinas_corrected','salinas_gt')
+    f.write(str(log)+"\n")
+
     # log = dumper.dump("base_data/SalinasA_corrected.mat", "base_data/SalinasA_gt.mat", "data/salinasA.csv",'salinasA_corrected','salinasA_gt')
     # f.write(str(log)+"\n")
