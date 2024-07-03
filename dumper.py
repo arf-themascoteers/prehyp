5bd5ed2acba86872760c3bb7965f785b9d191d15
aarahman <arif@themascoteers.com>
2024-07-03 20:25:53 +1000

    done

--- dumper.py
+++ dumper.py
@@ -13,10 +13,10 @@ def dump(X_file,gt_file,dump_file,X_key,gt_key):
     X = X.reshape(X.shape[0] * X.shape[1], X.shape[2])
     gt = gt.reshape(-1)
 
-    mask = (gt!=0)
-
-    gt = gt[mask]
-    X = X[mask]
+    # mask = (gt!=0)
+    #
+    # gt = gt[mask]
+    # X = X[mask]
 
     #scaler = MinMaxScaler()
     #X = scaler.fit_transform(X)
@@ -24,7 +24,8 @@ def dump(X_file,gt_file,dump_file,X_key,gt_key):
     columns = [str(i) for i in range(X.shape[1])] + ["class"]
     data = np.concatenate((X,gt.reshape(-1,1)), axis=1)
     df = pd.DataFrame(columns=columns, data=data)
-    mapping = {v: i for i, v in enumerate(sorted(df["class"].unique()))}
-    df["class"] = df["class"].map(mapping)
+    # mapping = {v: i for i, v in enumerate(sorted(df["class"].unique()))}
+    # df["class"] = df["class"].map(mapping)
+    mapping = ""
     df.to_csv(dump_file, index=False)
     return mapping
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import dumper
 if __name__ == '__main__':
     f = open("log.txt", "w")
 
-    log = dumper.dump("base_data/Indian_pines_corrected.mat", "base_data/Indian_pines_gt.mat", "data/indian_pines.csv",'indian_pines_corrected','indian_pines_gt')
-    f.write(str(log)+"\n")
+    # log = dumper.dump("base_data/Indian_pines_corrected.mat", "base_data/Indian_pines_gt.mat", "data/indian_pines.csv",'indian_pines_corrected','indian_pines_gt')
+    # f.write(str(log)+"\n")
 
     # log = dumper.dump("base_data/Pavia.mat", "base_data/Pavia_gt.mat", "data/pavia.csv",'pavia','pavia_gt')
     # f.write(str(log)+"\n")
